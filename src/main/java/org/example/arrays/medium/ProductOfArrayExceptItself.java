1ae8060d7fae7545d3dc03121bc291fb262dc4b6
Rahul Vedi <rahul.v7800@gmail.com>
2023-03-19 19:02:47 +0530

    Day 1(19-March-2023) : Product-of-array-except-itself

--- src/main/java/org/example/arrays/medium/ProductOfArrayExceptItself.java
+++ src/main/java/org/example/arrays/medium/ProductOfArrayExceptItself.java
@@ -1,11 +1,31 @@
 package org.example.arrays.medium;
 
+import org.example.arrays.ArrayUtils;
+
 public class ProductOfArrayExceptItself {
     public static void main(String[] args) {
 //        https://leetcode.com/problems/product-of-array-except-self/
-        
+        int[] arr = new int[]{-1, 1, 0, -3, 3};
+        ArrayUtils.display(productExceptSelf(arr));
     }
-    public int[] productExceptSelf(int[] nums) {
-        return null;
+
+    public static int[] productExceptSelf(int[] nums) {
+        int[] returnlist = new int[nums.length];
+        int zeroesCount = 0;
+        int product = 1;
+        for (int num : nums) {
+            if (num != 0) product = product * num;
+            else zeroesCount++;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                if (zeroesCount > 1) returnlist[i] = 0;
+                else returnlist[i] = product;
+            } else {
+                if (zeroesCount > 0) returnlist[i] = 0;
+                else returnlist[i] = product / nums[i];
+            }
+        }
+        return returnlist;
     }
 }
